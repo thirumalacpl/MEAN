f3aa27c1b1666cb84e8326448557b668dfd7463c
Thirumal <thirumal.acpl@gmail.com>
2024-07-14 11:31:51 +0530

    logout cookies clear

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -91,7 +91,14 @@ const login = asyncHandler(async (req, res) => {
     console.log(email, password);
   });
 
-const logout = async() => {};
+  const logout = asyncHandler(async (req, res) => {
+    res.cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    res.status(200).json({ message: "Logged out.." });
+  });
+  
 const getProfile = async() => {};
 const updateProfile = async() => {};
 
--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -8,6 +8,6 @@ router.post("/", createUser)
 router.post("/login", login)
 router.get("/profile", getProfile)
 router.put("/profile", updateProfile) /// update
-router.post("logout", logout)
+router.post("/logout", logout)
 
 export default router;
