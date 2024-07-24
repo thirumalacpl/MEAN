6181fe34e8ada9d76882c3d08ad44c6cf954eec9
Thirumal <thirumal.acpl@gmail.com>
2024-07-24 22:23:26 +0530

    ng-bootstrap n icon ivy error.. created login,forgot-pass,notfound components

--- Frontend/angular_jwt/src/app/app-routing.module.ts
+++ Frontend/angular_jwt/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerRootModule } from './features/customer/customer-root/customer-root.module';
+import { LoginComponent } from './core/components/login/login.component';
+import { ForgotPasswordComponent } from './core/components/forgot-password/forgot-password.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 const routes: Routes = [
+  {path: 'login', component:LoginComponent},
+  {path:'forgot-password', component: ForgotPasswordComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
   // {
   //   path: 'customer/customer-root',
   //   loadChildren: () =>
--- Frontend/angular_jwt/src/app/app.module.ts
+++ Frontend/angular_jwt/src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerRootComponent } from './features/customer/customer-root/customer-root.component';
 import { CustomerRootModule } from './features/customer/customer-root/customer-root.module';
+import { LoginComponent } from './core/components/login/login.component';
+import { ForgotPasswordComponent } from './core/components/forgot-password/forgot-password.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
+
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerRootComponent
+    CustomerRootComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CustomerRootModule
+    CustomerRootModule,
+    ReactiveFormsModule
+    // FontAwesomeModule,
+    // NgbModule
   ],
   providers: [],
   //schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
